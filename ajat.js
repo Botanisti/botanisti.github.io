6964f2c57acf02d35d1bf8d22660e130e52745be
Jari Harinen <jari.harinen@windowslive.com>
2023-03-26 23:52:31 +0300

    final

---
+++ ajat.js
@@ -0,0 +1,12 @@
+var aikaaKappaleet = "Aikaa Kappaleen Lähettämiseen";
+var aikaaArviointiin = "Aikaa Arvoinnin Lähettämiseen";
+var aikaaPisteytykseen = "Aikaa Pisteiden Lähettämiseen";
+// Declaring global variable outside the function
+myFunction();
+// Global variable accessed from
+// Within a function
+function myFunction() {
+   document.getElementById("aikaa").innerHTML = aikaaKappaleet;
+}
+// Changing value of global
+// Variable from outside of function
